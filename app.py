72e2a3f5903c68ea6532da92a098eb74f9442b78
Semaj Wiggins <semaj.wiggins@hgs.hiddengeniusproject.org>
2025-10-08 18:21:46 -0700

    Templates With Jinja2 Complete

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# A route to the home page of the app
 @app.route('/')
 def index():
     return "Hello World!"
 
+# A new route to the 'greet page of the app
+@app.route("/greet/<name>")
+def GreetPage(name):
+    return render_template("index.html", name=name)
+
 if __name__ == '__main__':
     app.run(debug = True)
+
